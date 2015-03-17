c7d23055c39f37149c9701b3c6b4f4b6281a6bb4
Maya Mathur <mmathur@stanford.edu>
2015-03-16 20:55:29 -0700

    Working version with n=200, obs=2, 16 workers. Only 1 sbatch file.

--- r/GENCOV/push_to_sherlock.R
+++ r/GENCOV/push_to_sherlock.R
@@ -1,6 +1,4 @@
 # this pushes the files from local GitHub repo to Sherlock
-# Test at 7:55 pm
-
 
 # push all the individual files
 system( "scp ~/Dropbox/QSU/Mathur/PCORI/PCORI_git/r/GENCOV/* mmathur@sherlock:/share/PI/manishad/genCov" )
@@ -9,5 +7,5 @@ system( "scp ~/Dropbox/QSU/Mathur/PCORI/PCORI_git/r/GENCOV/* mmathur@sherlock:/s
 system( "scp -r ~/Dropbox/QSU/Mathur/PCORI/PCORI_git/r/GENCOV/sbatch_files mmathur@sherlock:/share/PI/manishad/genCov" )
 
 # run them
-# sbatch /share/PI/manishad/genCov/sbatch_files/2.sbatch -p manishad
+# cd /share/PI/manishad/genCov/sbatch_files/2.sbatch
 # sbatch 2.sbatch -p manishad
